0b96118736776a00301c0e7e15600a20daff17a1
Vidusahan Perera <fc110559@foc.sjp.ac.lk>
2024-06-10 12:03:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = e.target.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
